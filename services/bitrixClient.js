b671097462e93717c88a3a1eb07aaa6adc514b81
devibuild <91279012+devibuild@users.noreply.github.com>
2021-10-29 14:50:26 -0300

    bitrix webhook back

--- services/bitrixClient.js
+++ services/bitrixClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const bitrixClient = axios.create({
-    baseURL: '',
+    baseURL: 'https://ibuild.bitrix24.com.br/rest/108/s01u2sxpt4nih11o/crm.deal.add.json',
     timeout: 5000,
     headers: {
         // 'X-Requested-With': 'XMLHttpRequest',
