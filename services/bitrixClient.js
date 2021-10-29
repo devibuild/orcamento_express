50ad1618cf4f7974e15766056dbf3af5726b477b
devibuild <91279012+devibuild@users.noreply.github.com>
2021-10-29 13:45:05 -0300

    removido webhook

--- services/bitrixClient.js
+++ services/bitrixClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const bitrixClient = axios.create({
-    baseURL: 'https://ibuild.bitrix24.com.br/rest/4/stwd1661o756mglv/crm.lead.add.json',
+    baseURL: '',
     timeout: 5000,
     headers: {
         // 'X-Requested-With': 'XMLHttpRequest',
